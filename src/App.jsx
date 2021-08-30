e82c9465b06517ba53b73d85dbf2c5513a149449
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-08-30 19:01:24 +0545

    everrything good

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Works from "./components/works/Works";
 import Contact from "./components/contact/Contact";
 import "./app.scss"
 import {useState} from "react";
+import Menu from "./components/menu/Menu";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
     <Topbar menuOpen={menuOpen} setMenuOpen= {setMenuOpen}/>
+    <Menu/>
      <div className="sections">
      <Intro/>
        <Portfolio/>
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,24 @@
+import "./menu.scss"
+export default function () {
+    return (
+        <div className ="menu">
+            <ul>
+                <li>
+                    <a href="#intro">Home</a>
+                </li>
+                <li>
+                    <a href="#portfolio">Portfolio</a>
+                </li>
+                <li>
+                    <a href="#works">Works</a>
+                </li>
+                <li>
+                    <a href="#testimonials">Testimonials</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
