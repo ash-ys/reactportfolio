080739cc86efb228b23e3241ce907db1bd203fb6
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-08-31 08:17:53 +0545

    menu dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import Works from "./components/works/Works";
 import Contact from "./components/contact/Contact";
 import "./app.scss"
 import {useState} from "react";
-import Menu from "./components/menu/Menu";
+import Menu from "./components/menu/Menu"
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
     <Topbar menuOpen={menuOpen} setMenuOpen= {setMenuOpen}/>
-    <Menu/>
+    <Menu menuOpen={menuOpen} setMenuOpen= {setMenuOpen}/>
      <div className="sections">
      <Intro/>
        <Portfolio/>
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,21 +1,21 @@
 import "./menu.scss"
-export default function () {
+export default function Menu({menuOpen,setMenuOpen}) {
     return (
-        <div className ="menu">
+        <div className ={"menu "+(menuOpen && "active")}>
             <ul>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#intro">Home</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#portfolio">Portfolio</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#works">Works</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#testimonials">Testimonials</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#contact">Contact</a>
                 </li>
             </ul>
