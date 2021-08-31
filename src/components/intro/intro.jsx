f77ee2a3362b152053cbb61abba23ca11158ce76
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-08-31 09:40:23 +0545

    profile

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -3,7 +3,11 @@ import "./intro.scss"
 export default function intro() {
     return (
         <div className="intro" id = "intro">
-            <div className="left"></div>
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/me.png" alt="" />
+                </div>
+            </div>
             <div className="right"></div>
         </div>
     )
