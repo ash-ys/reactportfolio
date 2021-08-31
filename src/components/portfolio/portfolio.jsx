caa199f9f7b5c34daf4e5107878184f0ea55533b
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-08-31 13:35:21 +0545

    portfolio

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,7 +3,20 @@ import "./portfolio.scss"
 export default function portfolio() {
     return (
         <div className="portfolio" id ="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li className ="active">Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/Savings.svg" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
         </div>
     )
 }
