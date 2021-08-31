e5e18eae2076b51569820d442d924be78fa7f001
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-08-31 08:39:58 +0545

    flex

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -3,7 +3,8 @@ import "./intro.scss"
 export default function intro() {
     return (
         <div className="intro" id = "intro">
-        
+            <div className="left"></div>
+            <div className="right"></div>
         </div>
     )
 }
