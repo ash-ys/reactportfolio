1c1810e7962259068a0bbd8c7945a9201b06df80
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-08-31 18:57:44 +0545

    react hook

--- src/components/portfoliolist/Portfoliolist.jsx
+++ src/components/portfoliolist/Portfoliolist.jsx
@@ -1,8 +1,9 @@
 import "./portfolioList.scss";
 
-export default function PortfolioList({title, active, setSelected}) {
+export default function PortfolioList({id, title, active, setSelected}) {
     return (
-        <li className={active ? "portfolioList active" : "portfolioList"} onClick={()=> setSelected()}>
+        <li className={active ? "portfolioList active" : "portfolioList"}
+         onClick={()=> setSelected(id)}>
             {title}
         </li>
     )
