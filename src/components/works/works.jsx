84e759821065af4eabfa80b236ae1c498e634725
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-09-02 13:37:53 +0545

    works

--- src/components/works/works.jsx
+++ src/components/works/works.jsx
@@ -3,6 +3,14 @@ import "./works.scss"
 export default function works() {
     return (
         <div className="works"  id ="works">
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left"></div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
