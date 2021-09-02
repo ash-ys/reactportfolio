132aa993a0b037288ecbb1b458f73f08bc3d1f30
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-09-02 17:15:19 +0545

    contact changes

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,9 +2,18 @@ import "./contact.scss"
 
 export default function contact() {
     return (
-        <div className = "contact" id ="contact"> 
-            
-            
+        <div className = "contact" id ="contact">
+            <div className="left">
+                <img src = "assets/shake.svg" alt =""/>
+            </div>
+            <div className="right">
+                <h2>Contact</h2>
+                <form >
+                    <input type ="text" placeholder ="Email"/>
+                    <textarea placeholder="message"></textarea>
+                    <button  type="submit">Send</button>
+                </form>
+            </div>
         </div>
     )
 }
