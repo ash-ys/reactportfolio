4c35d527a481dba5e2fc8245a05400a0752874d9
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-09-02 14:59:29 +0545

    testimonials

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -3,7 +3,23 @@ import "./testimonials.scss"
 export default function testimonials() {
     return (
         <div className = "testimonials"  id ="testimonials">
-            
+            <h1>Testimonials</h1>
+            <div className="container">
+                <div className="card">
+                    <div className="top">
+                        <img src="assets/right-arrow.png"className="left" alt="" />
+                        <img src="assets/bank.svg" alt="" />
+                        <img src="assets/youtube.png" alt="" />
+                    </div>
+                    <div className="center">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </div>
+                    <div className="bottom">
+                        <h3>Saugat</h3>
+                        <h4>CEO of Lalim</h4>
+                    </div>
+                    </div>       
+            </div>
         </div>
     )
 }
