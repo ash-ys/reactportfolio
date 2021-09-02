8d48f9554a072a09bb86d144e8eec18f55d6ef4c
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-09-02 13:44:30 +0545

    works

--- src/components/works/works.jsx
+++ src/components/works/works.jsx
@@ -6,7 +6,22 @@ export default function works() {
             <div className="slider">
                 <div className="container">
                     <div className="item">
-                        <div className="left"></div>
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/mobile.png" alt="" />
+                                </div>
+                                <h2>Title</h2>
+                                <p>Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Maiores et nisi 
+                                    qui magni inventore aliquid hic vel
+                                    perferendis quia quisquam laboriosam,
+                                    esse pariatur eos placeat ullam
+                                    minus incidunt voluptatum laborum.
+                                    </p>
+                                    <span>Projects</span>
+                            </div>
+                        </div>
                         <div className="right"></div>
                     </div>
                 </div>
