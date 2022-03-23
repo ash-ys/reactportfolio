9db3e6aa2679351c4e5a3e2946fb0dcd25418241
Ashish <53848069+ash-ys@users.noreply.github.com>
2022-03-23 12:20:12 +0545

    ya

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -2,12 +2,12 @@ import "./testimonials.scss"
 
 export default function testimonials() {
     return (
-        <div className = "testimonials"  id ="testimonials">
+        <div className="testimonials" id="testimonials">
             <h1>Testimonials</h1>
             <div className="container">
                 <div className="card">
                     <div className="top">
-                        <img src="assets/right-arrow.png"className="left" alt="" />
+                        <img src="assets/right-arrow.png" className="left" alt="" />
                         <img src="assets/bank.svg" className="user" alt="" />
                         <img src="assets/youtube.png" className="right" alt="" />
                     </div>
@@ -16,10 +16,10 @@ export default function testimonials() {
                         His approach in content creation is very good and simple.
                     </div>
                     <div className="bottom">
-                        <h3>Ram</h3>
-                        <h4>CEO of Lalim</h4>
+                        <h3>Anish</h3>
+                        <h4>Writer</h4>
                     </div>
-                    </div>       
+                </div>
             </div>
         </div>
     )
